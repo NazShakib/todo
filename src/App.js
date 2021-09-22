c4fb844a93b3f5b4bf9baa4191022f85318662a4
Nazmus Sakib <20153768+NazShakib@users.noreply.github.com>
2021-09-22 12:28:15 +0600

    add webcam

--- src/App.js
+++ src/App.js
@@ -1,24 +1,133 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
+import ReactDOM from 'react-dom'
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
 import './App.css';
 
 // import components from 
 import Form from './components/Form';
 import TodoList from './components/TodoList';
+import camera from './camera.js';
+import Docs from './docs/';
+
+// image capture module
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
 
 function App() {
   const [inputText,setInputText]= useState("");
   const [todo, setTodo] = useState([]);
+
+  const Image = ({ data }) => <img class="center" src={`data:image/jpeg;base64,${data}`} />
+
+    function handleTakePhoto (dataUri) {
+      // Do stuff with the photo...
+      var imageURL = dataUri.split(",")[1];
+      ReactDOM.render(<Image data={imageURL} />, document.getElementById('img-container'))
+    }
+
+    function handleCameraStart (stream) {
+      console.log('handleCameraStart');
+      <XYPlot
+      width={stream.width}
+      height={stream.height}>
+      <HorizontalGridLines />
+      
+      <LineSeries
+        data={[
+          {x: 1, y: 10},
+          {x: 1, y: 5},
+          {x: 1, y: 15}
+        ]}/>
+      <XAxis />
+      <YAxis />
+    </XYPlot>
+    }
+  
+    function handleCameraStop () {
+      console.log('handleCameraStop');
+    }
+
   return (
     <div className="App">
+      
       <header>
-        <h1>Todo List Demo</h1>
+        <h1>Capture Your report</h1>
       </header>
-      <Form 
+      <div class="container">
+      <div class="parent-shades">
+
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+      <div class='shades'></div>
+
+
+
+
+      </div>
+        <Camera 
+        idealResolution = {{width: 640, height: 480}}
+        onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
+        //onTakePhotoAnimationDone = { (dataUri) => { handleTakePhotoAnimationDone(dataUri); } }
+        imageCompression = {0.97}
+        isMaxResolution = {true}
+        isImageMirror = {false}
+        onCameraStart = { (stream) => { handleCameraStart(stream); } }
+        onCameraStop = { () => { handleCameraStop(); } }
+      />
+
+      
+    </div>
+    <form >
+      <div id="img-container">
+
+      </div>
+      <br/>
+      <button type='submit' id="button-submit">Save image</button>
+    </form>
+
+ 
+ 
+  {/* <Form 
+      
       todo={todo} 
       setTodo={setTodo} 
       setInputText={setInputText} 
       inputText={inputText} />
-      <TodoList todo={todo} setTodo={setTodo}/>
+      <TodoList todo={todo} setTodo={setTodo}/> */}
     </div>
   );
 }
